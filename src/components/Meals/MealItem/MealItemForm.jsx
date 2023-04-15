60a15e17b10e777a9c701e95718ab5e471040c5b
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-04-15 03:10:44 +0300

    input added

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import './MealItemForm.css';
+import Input from '../../UI/Input';
 
-const MealItemForm = () => {
-  return <div></div>;
+const MealItemForm = (props) => {
+  return (
+    <form className='form'>
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
